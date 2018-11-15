50f36a62a819692c81e338cd4a2ff1b14cf9dac2
unknown <tkemp@wisc.edu>
2018-11-15 15:33:49 -0600

    Added a read_data file as well as fleshed out more of the main python file

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import numpy as np
 import tensorflow as tf
+import read_data as rd
 
-def read_data():
-	return 0, 0
+def get_sub(data, filter, col):
+	filtered = []
+	for x in data:
+		if(x[col] == filter):
+			filtered.append(x)
+	return filtered
 
 def model():
 	learn_rate = 0.001#need to determine an optimal learning rate
-	lam = 0.0001#should very this in order to determine impact on accuracy as well as fairness
+	batch_size = 1#need to determine a good batch size based on total dataset size
+	epochs = 20#need to determine a good epoch amount
+	lam = 0.0001#should vary this in order to determine impact on accuracy as well as fairness
 	#procedure for choosing optimal lam for a dataset
-	features, lables = read_data()
+	features, lables = rd.read_data()
 	#shuffle data then take 80% for training 20% for validation
+	print(features)
 
 	num_features = len(features[0])#determine num_features based on 
 
@@ -25,18 +33,42 @@ def model():
 		output = x_features * weights + bias
 
 	with tf.name_scope("loss_function"):
-		reg = tf.math.exp()
-		loss = tf.losses.mean_squared_error(labels, output) + reg#logic for loss function goes here
+		with tf.name_scope("placeholder"):
+			lam = tf.placeholder(tf.float32, shape=(1,1))
+
+		with tf.name_scope("expected_loss_sub"):
+			sub_loss = tf.placeholder(tf.float32, shape=(1,1))
+
+		with tf.name_scope("expected_loss_ov"):
+			ov_loss = tf.placeholder(tf.float32, shape=(1,1))
+
+		with tf.name_scope("regularizer"):
+			exp = tf.exp(sub_loss - ov_loss)
+			reg = tf.log(1+exp)
+
+		loss = tf.losses.mean_squared_error(labels, output) + lam * reg
 
 	with tf.name_scope("optimizer"):
-		opt = GradientDescentOptimizer(learning_rate=learn_rate).minimize(loss)
+		opt = GradientDescentOptimizer(
+			learning_rate=learn_rate).minimize(loss)
 
 
 	with tf.Session as sess:
 		sess.run(tf.initializers.global_variables())
 
 		for batch in batches:
-			sess.run(opt)
+			for epoch in range(num_epochs):
+				ov_loss = sess.run(loss, 
+					feed_dict={x_features:batch["x"], labels:batch["y"]})
+				sub_loss = sess.run(loss, 
+					feed_dict={x_features:get_sub(batch["x"], group), 
+					labels:get_sub(batch["y"])})
+
+				sess.run(opt, 
+					feed_dict={x_features:batch["x"], 
+					labels:batch["y"], 
+					sub_loss:group_loss, 
+					ov_loss:overall_loss})
 
 		#perform testing
 
@@ -46,3 +78,6 @@ def model():
 		#should include training logic
 		#should include testing logic at the end
 		pass
+
+model()
+
---
+++ read_data.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+
+def read_data():
+	x = np.array([[1, 2, 3],[3, 5, 7]])
+	y = np.array([[1],[0]])
+	return x, y
