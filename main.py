43cf0df5fc973dee82d3cb41a3595cd08d5c3112
unknown <tkemp@wisc.edu>
2018-11-15 16:49:00 -0600

    added summaries, fixed bugs in read_data.py

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import numpy as np
 import tensorflow as tf
 import read_data as rd
 
-def get_sub(data, filter, col):
-	filtered = []
-	for x in data:
-		if(x[col] == filter):
-			filtered.append(x)
-	return filtered
-
 def model():
 	learn_rate = 0.001#need to determine an optimal learning rate
 	batch_size = 1#need to determine a good batch size based on total dataset size
-	epochs = 20#need to determine a good epoch amount
+	num_epochs = 20#need to determine a good epoch amount
+	num_groups = 2
 	lam = 0.0001#should vary this in order to determine impact on accuracy as well as fairness
+	groups = [0, 1]# should have values of group
+	column = 2#will need to obtain from dataset
 	#procedure for choosing optimal lam for a dataset
-	features, lables = rd.read_data()
+	features, labels = rd.read_data()
+	batches = rd.get_batches(features, labels, batch_size)
 	#shuffle data then take 80% for training 20% for validation
-	print(features)
 
 	num_features = len(features[0])#determine num_features based on 
 
@@ -30,54 +26,53 @@ def model():
 	with tf.name_scope("model"):
 		weights = tf.Variable(tf.random_normal([num_features, 1]))
 		bias = tf.Variable(0.0)
-		output = x_features * weights + bias
+		output = tf.matmul(x_features, weights) + bias
+		weight_summary = tf.summary.histogram("weight_summary", weights)
+		bias_summary = tf.summary.histogram("bias_summary", bias)
 
 	with tf.name_scope("loss_function"):
 		with tf.name_scope("placeholder"):
-			lam = tf.placeholder(tf.float32, shape=(1,1))
+			lam_value = tf.placeholder(tf.float32, shape=(1,1))
 
 		with tf.name_scope("expected_loss_sub"):
-			sub_loss = tf.placeholder(tf.float32, shape=(1,1))
+			sub_loss = tf.placeholder(tf.float32, shape=(batch_size,1))
 
 		with tf.name_scope("expected_loss_ov"):
-			ov_loss = tf.placeholder(tf.float32, shape=(1,1))
+			ov_loss = tf.placeholder(tf.float32, shape=(batch_size,1))
 
 		with tf.name_scope("regularizer"):
 			exp = tf.exp(sub_loss - ov_loss)
 			reg = tf.log(1+exp)
 
-		loss = tf.losses.mean_squared_error(labels, output) + lam * reg
+		loss = tf.losses.mean_squared_error(labels, output) + lam_value * reg
+		tf.summary.scalar("loss_summary", loss)
 
 	with tf.name_scope("optimizer"):
-		opt = GradientDescentOptimizer(
+		opt = tf.train.GradientDescentOptimizer(
 			learning_rate=learn_rate).minimize(loss)
 
 
-	with tf.Session as sess:
+	with tf.Session() as sess:
 		sess.run(tf.initializers.global_variables())
+		summary = tf.summary.merge_all()
+		writer = tf.summary.FileWriter("graph", sess.graph)
 
 		for batch in batches:
 			for epoch in range(num_epochs):
+				sub_x, sub_y = rd.get_sub(batch, groups[epoch % num_groups], column)
 				ov_loss = sess.run(loss, 
 					feed_dict={x_features:batch["x"], labels:batch["y"]})
 				sub_loss = sess.run(loss, 
-					feed_dict={x_features:get_sub(batch["x"], group), 
-					labels:get_sub(batch["y"])})
+					feed_dict={x_features:sub_x, 
+					labels:sub_y})
 
-				sess.run(opt, 
+				summary, _ = sess.run([summary, opt], 
 					feed_dict={x_features:batch["x"], 
 					labels:batch["y"], 
 					sub_loss:group_loss, 
 					ov_loss:overall_loss})
-
-		#perform testing
-
-
-		#initialize all variables
-		#should include a tf writer to store results 
-		#should include training logic
-		#should include testing logic at the end
-		pass
+				if(epoch == num_epochs - 1):
+					writer.add_summary(summary)
 
 model()
 
--- read_data.py
+++ read_data.py
@@ -2,6 +2,37 @@ import tensorflow as tf
 import numpy as np
 
 def read_data():
-	x = np.array([[1, 2, 3],[3, 5, 7]])
-	y = np.array([[1],[0]])
+	x = np.array([[1.0, 2.0, 3.0],[3.0, 5.0, 7.0]])
+	y = np.array([[1.0],[0.0]])
 	return x, y
+
+def get_sub(data, filter, col):
+	filteredx = []
+	filteredy = []
+	x = data["x"]
+	y = data["y"]
+
+	for i in range(len(x)):
+		if(x[i][col] == filter):
+			filteredx.append(x[i])
+			filteredy.append(labels[i])
+
+	return {"x":filteredx, "y":filteredy}
+
+def get_batches(features, labels, batch_size):
+	num_batches = int(len(features) / batch_size)
+	batches = []
+	lower = 0
+	upper = batch_size
+
+	if(len(features) <= batch_size):
+		return [{"x":features, "y":labels}]
+
+	for batch in range(num_batches):
+		batches.append({"x":features[lower:upper], "y":labels[lower:upper]})
+		lower = upper
+		upper += batch_size
+
+	#last batch not getting added
+
+	return batches
