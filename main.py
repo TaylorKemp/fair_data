7b32a01759ccdcc86c03850ea5811afdaacfdde0
unknown <tkemp@wisc.edu>
2018-11-29 23:25:13 -0600

    Fixed working model to run type python run.py. Will run script that tries various regularization parameters

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import numpy as np
 import tensorflow as tf
 import read_data as rd
+import pandas as pd
+import matplotlib.pyplot as plt
 
-file="clean_dataset.txt"
-trial = "1"
+file="compass/propublicaCompassRecividism_data_fairml.csv/propublica_data_for_fairml.csv"#"clean_dataset.txt"
+trial = "3"
 
-def model():
+def read_csv(filename=None):
+	if filename == None:
+		return
+
+	df = np.genfromtxt(filename, delimiter=',')
+	#data = np.genfromtxt(filename, delimiter=',')
+
+	df = np.delete(df, 0, 0)
+	np.random.shuffle(df)
+	train = int(len(df) * .8)
+	y_values = np.reshape(df[:train,0], (-1, 1))
+	y_values[np.where(y_values == 0)] = -1.0
+	x_values = df[:train, 1:]
+	y_test = np.reshape(df[train:, 0], (-1, 1))
+	y_test[np.where(y_test == 0)] = -1.0
+	x_test = df[train:,1:]
+
+	return x_values, y_values, x_test, y_test
+
+
+def model(lam=0.0, valid=False):
 	tf.summary.FileWriterCache.clear()
-	learn_rate = 0.0001#need to determine an optimal learning rate
-	batch_size = 25#need to determine a good batch size based on total dataset size
-	num_epochs = 10#need to determine a good epoch amount
-	iterations = 3
+	learn_rate = 0.01#need to determine an optimal learning rate
+	batch_size = 200#need to determine a good batch size based on total dataset size
+	num_epochs = 5#need to determine a good epoch amount
+	iterations = 10
 	num_groups = 2
-	lam = 0.0#should vary this in order to determine impact on accuracy as well as fairness
 	groups = [0, 1]# should have values of group
-	column = 2#will need to obtain from dataset
+	column = 4#will need to obtain from dataset
 	#procedure for choosing optimal lam for a dataset
-	features, labels,test_features, test_labels = rd.read_data(filename=file)
+	features, labels,test_features, test_labels = read_csv(file)#rd.read_data(filename=file)
 	#need to shuffle the data. This is causing a problem with the second half overriding the first half
 	#hence causing problems
 	batches = rd.get_batches(features, labels, batch_size)
@@ -24,78 +45,78 @@ def model():
 
 	num_features = len(features[0])#determine num_features based on 
 
+	with tf.Graph().as_default() as graph:
+		with tf.name_scope("features"):
+			with tf.name_scope("subGroup"):
+				len_group = tf.placeholder(tf.float32, shape=(), name="group_len")
+				group_x = tf.placeholder(tf.float32, shape=(None, num_features))
 
-	with tf.name_scope("features"):
-		with tf.name_scope("subGroup"):
-			len_group = tf.placeholder(tf.float32, shape=(), name="group_len")
-			group_x = tf.placeholder(tf.float32, shape=(None, num_features))
+			with tf.name_scope("remainingData"):
+				len_rest = tf.placeholder(tf.float32, shape=(), name="rest_len")
+				rest_of_x = tf.placeholder(tf.float32, shape=(None, num_features))
 
-		with tf.name_scope("remainingData"):
-			len_rest = tf.placeholder(tf.float32, shape=(), name="rest_len")
-			rest_of_x = tf.placeholder(tf.float32, shape=(None, num_features))
+		with tf.name_scope("labels"):
+			with tf.name_scope("subGroup"):
+				group_y = tf.placeholder(tf.float32, shape=(None, 1))
 
-	with tf.name_scope("labels"):
-		with tf.name_scope("subGroup"):
-			group_y = tf.placeholder(tf.float32, shape=(None, 1))
+			with tf.name_scope("remainingData"):
+				rest_of_y = tf.placeholder(tf.float32, shape=(None, 1))
 
-		with tf.name_scope("remainingData"):
-			rest_of_y = tf.placeholder(tf.float32, shape=(None, 1))
+			labels = tf.concat([rest_of_y, group_y], 0)
 
-		labels = tf.concat([rest_of_y, group_y], 0)
+		with tf.name_scope("model"):
+			weights = tf.Variable(tf.random_normal([num_features, 1]), name="weights")
+			bias = tf.Variable(0.0, name="bias")
 
-	with tf.name_scope("model"):
-		weights = tf.Variable(tf.random_normal([num_features, 1]), name="weights")
-		bias = tf.Variable(0.0, name="bias")
+			with tf.name_scope("remainingData"):
+				output_rest = tf.matmul(rest_of_x, weights) + bias
 
-		with tf.name_scope("remainingData"):
-			output_rest = tf.matmul(rest_of_x, weights) + bias
+			with tf.name_scope("subGroup"):
+				output_group = tf.matmul(group_x, weights) + bias
 
-		with tf.name_scope("subGroup"):
-			output_group = tf.matmul(group_x, weights) + bias
+			output = tf.concat([output_rest, output_group], 0)
 
-		output = tf.concat([output_rest, output_group], 0)
+			with tf.name_scope("summaries"):
+				weight_summary = tf.summary.histogram("weightSummary", weights)
+				bias_summary = tf.summary.histogram("bias_summary", bias)
 
-		with tf.name_scope("summaries"):
-			weight_summary = tf.summary.histogram("weightSummary", weights)
-			bias_summary = tf.summary.histogram("bias_summary", bias)
+		with tf.name_scope("lossFunction"):
+			with tf.name_scope("placeholder"):
+				lam_value = tf.constant(lam)
 
-	with tf.name_scope("lossFunction"):
-		with tf.name_scope("placeholder"):
-			lam_value = tf.constant(lam)
+			with tf.name_scope("expected_loss_sub"):
+				zeros = tf.zeros((1,num_features), dtype=tf.float32)
+				group_loss = tf.losses.hinge_loss(group_y, output_group)
 
-		with tf.name_scope("expected_loss_sub"):
-			zeros = tf.zeros((1,num_features), dtype=tf.float32)
-			group_loss = tf.losses.mean_squared_error(group_y, output_group)
+			squared_error = tf.losses.hinge_loss(labels, output) + 0.001 * tf.norm(weights, ord=2)
 
-		squared_error = tf.losses.mean_squared_error(labels, output)
+			with tf.name_scope("regularizer"):#need to scale these by the size of the other
+				exp = len_rest * group_loss - len_group * squared_error
+				reg = tf.nn.softplus(exp)
+			
+			loss = squared_error + lam_value * reg
 
-		with tf.name_scope("regularizer"):#need to scale these by the size of the other
-			exp = len_rest * group_loss - len_group * squared_error
-			reg = tf.nn.softplus(exp)
-		
-		loss = squared_error + lam_value * reg
+			with tf.name_scope("summaries"):
+				loss_summary = tf.summary.scalar("lossSummary", loss)
 
-		with tf.name_scope("summaries"):
-			loss_summary = tf.summary.scalar("lossSummary", loss)
+		with tf.name_scope("optimizer"):
+			optim = tf.train.GradientDescentOptimizer(
+				learning_rate=learn_rate)
+			grad = optim.compute_gradients(loss)
+			opt = optim.minimize(loss)
 
-	with tf.name_scope("optimizer"):
-		optim = tf.train.GradientDescentOptimizer(
-			learning_rate=learn_rate)
-		grad = optim.compute_gradients(loss)
-		opt = optim.minimize(loss)
+		saver = tf.train.Saver()
+		iteration = 0
 
-	saver = tf.train.Saver()
-	iteration = 0
-
-	with tf.Session() as sess:
+	with tf.Session(graph=graph) as sess:
 		merged = tf.summary.merge_all()
-		writer = tf.summary.FileWriter("graph/trial" + trial, sess.graph)
+		writer = tf.summary.FileWriter("graph/lam=" + str(lam), sess.graph)
 
 		sess.run(tf.initializers.global_variables())
 		for rnd in range(iterations):
 			for batch in batches:
 				for epoch in range(num_epochs):
-					sub_x, sub_y, rest_x, rest_y = rd.get_groups(batch, groups[epoch % num_groups], column)
+					sub_x, sub_y, rest_x, rest_y = rd.get_groups(batch, groups[1], column)
 
 					if len(sub_y) < 1:
 						sub_y = np.reshape([sess.run(bias)], (1, 1))
@@ -113,29 +134,22 @@ def model():
 						len_rest: len(rest_y)})
 
 					if(epoch % 5 == 0):
-						#print("summary written")
 						writer.add_summary(summary, global_step=iteration)
-						out_val = sess.run(output_group, 
-									feed_dict={group_x:test_features})
+						iteration += 1
 
-						num_correct = 0
-						out_val = np.reshape(out_val, (1, -1))
+			out_val = np.matmul(test_features, sess.run(weights)) + sess.run(bias)
 
-						for i in range(len(test_labels)):
-							if(out_val[0][i] > 0 and test_labels[i] == 1):
-								num_correct += 1
-							elif(out_val[0][i] <= 0 and test_labels[i] == -1):
-								num_correct += 1
+			num_correct = 0
 
-						print(sess.run(
-							tf.losses.mean_squared_error(
-								tf.reshape(out_val, shape=(1, 750)), tf.reshape(test_labels, shape=(1, 750)))))
+			out_val = np.reshape(out_val, (1, -1))
+			test_labels = np.reshape(test_labels, (1, -1))
 
-						print("Total Correct:")
-						print(num_correct)
-						print("Out of:")
-						print(len(test_labels))
-						iteration = iteration + 1
+			for i in range(len(test_labels[0])):
+				if(out_val[0][i] > 0 and test_labels[0][i] == 1):
+					num_correct += 1
+				elif(out_val[0][i] <= 0 and test_labels[0][i] == -1):
+					num_correct += 1
 
-
-model()
+		w = sess.run(weights)
+		b = sess.run(bias)
+	return w, b, test_labels, test_features
---
+++ run.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import numpy as np
+import main as mn
+import read_data as rd
+import matplotlib.pyplot as plt
+
+
+def run_multiple():
+	lambdas = [0.0, 0.001, 0.01, 0.1, 0.5, 1.0, 2.0, 4.0, 5.0, 10.0]
+
+	acc_afr = ()
+	acc_other = ()
+	acc_overall = ()
+
+	for lam in lambdas:
+		if(lam == 0.001):
+			weights, bias, test_labels, test_features = mn.model(lam, True)
+		else:
+			weights, bias, test_labels, test_features = mn.model(lam, True)
+
+		weights = np.copy(weights)
+		bias = np.copy(bias)
+		test_labels = np.copy(test_labels)
+		test_features = np.copy(test_features)
+
+		test_labels	= np.reshape(test_labels, (-1, 1))
+		afr_feat, afr_lab, rest_feat, rest_lab = rd.get_groups({"x":test_features, "y":test_labels}, 1, 4)
+
+		acc_afr += (get_accuracy(afr_feat, weights, bias, afr_lab), )
+		acc_other += (get_accuracy(rest_feat, weights, bias, rest_lab), )
+		acc_overall += (get_accuracy(test_features, weights, bias, test_labels), )
+
+	plot_values(acc_afr, acc_other, acc_overall)
+
+def get_accuracy(x, w, b, y):
+	out_val = np.matmul(x, w) + b
+
+	num_correct = 0
+
+	out_val = np.reshape(out_val, (1, -1))
+	test_labels = np.reshape(y, (1, -1))
+
+	for i in range(len(test_labels[0])):
+		if(out_val[0][i] > 0 and test_labels[0][i] == 1):
+			num_correct += 1
+		elif(out_val[0][i] <= 0 and test_labels[0][i] == -1):
+			num_correct += 1
+
+	return float(num_correct) / len(y)
+
+
+def plot_values(afro, rest, oval):
+	#afro = (60.62, 57.10, 60.86, 59.59, 60.16, 54.12, 44.67, 45.88, 48.41, 44.61)
+	#rest = (66.10, 68.91, 64.86, 67.78, 58.87, 41.72, 40.87, 41.72, 38.35, 38.60)
+	#oval = (63.24, 62.75, 62.83, 63.56, 59.51, 48.18, 42.83, 43.89, 43.48, 41.62)
+
+	ind = np.arange(len(afro))  # the x locations for the groups
+	width = 0.3  # the width of the bars
+
+	fig, ax = plt.subplots()
+	rects1 = ax.bar(ind - width, afro, width, color='SkyBlue', label='African-American')
+	rects2 = ax.bar(ind, rest, width, color='IndianRed', label='other')
+	rects3 = ax.bar(ind + width, oval, width, color='green', label='overall')
+
+	# Add some text for labels, title and custom x-axis tick labels, etc.
+	ax.set_ylabel('Accuracy(%)')
+	ax.set_xlabel('Regularization Parameter')
+	ax.set_title('Affect of Regularization Parameter on Accuracy')
+	ax.set_xticks(ind)
+	ax.set_xticklabels(('0.0', '0.001', '0.01', '0.1', '0.5', '1.0', '2.0', '4.0', '5.0', '10.0'))
+	ax.legend()
+
+	plt.show()
+
+	print("done")
+
+
+run_multiple()
